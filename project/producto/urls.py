86ba14a776342b5c06f1ffc25a3d7b46e1a2f2d0
Francisco Acosta <franciscoaag12@gmail.com>
2024-05-05 17:04:21 -0300

    add: formularios a producto

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-app_name = "producto"
 from . import views
+
+app_name = "producto"
 
 urlpatterns = [
     path("home/", views.home, name= "home"),
+    path("productocategoria/create/", views.productocategoria_create, name="productocategoria_create"),
+    
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
-from . import models
+from . import models, forms
 
 def home(request):
+    # if request.GET["consulta"]:
+    #     consulta = request.GET["consulta"]
+    #     query = models.ProductoCategoria.objects.filter(nombre__icontains=consulta)
+    # else:
+    query = models.ProductoCategoria.objects.all()
+        
+    context = {"productos": query}
+    return render(request, "producto/index.html", context)
+
     consulta_productos = models.ProductoCategoria.objects.all()
     context = {"productos": consulta_productos} # clientes es la clave
     return render(request, "producto/index.html", context)
+
+def productocategoria_create(request):
+    if request.method == "POST":
+        form = forms.ProductoCategoriaForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("producto:home")
+    else: # request.method == "GET"
+        form = forms.ProductoCategoriaForm()
+    return render(request, "producto/productocategoria_create.html", context={"form": form})
